65410962ba1ad27293e64e3a1c7f351506c93dbd
tong <yutongl@umich.edu>
2019-06-29 11:52:42 +0800

    try

---
+++ src/main/java/com/yutongwenda/wenda/Controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.yutongwenda.wenda.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return"Hello Yutong";
+    }
+}
